18aa6c179ae1cb9de4bf5925ec2e1d05d2d9cfc3
Jakub Scholz <jakub@scholz.cz>
2016-03-31 09:48:12 +0200

    Refactoring of examples based on C++ binding

---
+++ cpp_binding/Amqp10BroadcastReceiver.py
@@ -0,0 +1,64 @@
+#! /usr/bin/python
+
+from Options import Options
+from qpid_messaging import *
+
+
+class Amqp10BroadcastReceiver:
+    block_size = 100
+    capacity = 1000
+
+    def __init__(self, options):
+        self.options = options
+        self.broadcast_address = "broadcast." + self.options.accountName + ".TradeConfirmation; " \
+                                 "{ node: { type: queue } , create: never , mode: consume , assert: never }"
+        self.message_counter = 0
+
+    def run(self):
+        message_counter = 0
+
+        try:
+            connection = Connection("amqp:ssl:" + self.options.hostname + ":" + str(self.options.port), protocol="amqp1.0")
+            connection.setOption("sasl_mechanisms", "EXTERNAL")
+            connection.setOption("transport", "ssl")
+            connection.setOption("heartbeat", 60)
+
+            connection.open()
+            session = connection.session()
+            receiver = session.receiver(self.broadcast_address)
+            receiver.capacity = self.capacity
+
+            while True:
+                received_message = None
+
+                try:
+                    received_message = receiver.fetch(timeout=self.options.timeout)
+                except Empty:
+                    session.acknowledge(sync=True)
+                    print "-I- No message received for ", self.options.timeout, " seconds"
+                    break
+
+                self.message_counter += 1
+                print "-I- Received message containing: ", received_message.content
+
+                if self.message_counter % self.block_size == 0:
+                    session.acknowledge(sync=True)
+
+            print "-I- ", self.message_counter, " messages received"
+
+            receiver.close()
+            session.close()
+            connection.close()
+        except MessagingError, m:
+            print "-E- Caught exception: ", m
+
+
+if __name__ == "__main__":
+    hostname = "ecag-fixml-simu1.deutsche-boerse.com"
+    port = 10170
+    accountName = "ABCFR_ABCFRALMMACC1"
+
+    opts = Options(hostname, port, accountName)
+
+    br = Amqp10BroadcastReceiver(opts)
+    br.run()
--- run_tests.py
+++ run_tests.py
@@ -1,10 +1,12 @@
 import unittest
 import sys
 import tests.test_PurePython
+import tests.test_CppBinding
 
 if __name__ == '__main__':
     suites = []
     suites.append(unittest.TestLoader().loadTestsFromModule(tests.test_PurePython))
+    suites.append(unittest.TestLoader().loadTestsFromModule(tests.test_CppBinding))
     allTests = unittest.TestSuite(suites)
 
     result = unittest.TextTestRunner(verbosity=2).run(allTests)
